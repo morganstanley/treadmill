22fe2cffd9be74e1f33d856e1ab90494a076102a
Ankita Bhopatkar <bankita@thoughtworks.com>
2017-05-02 12:41:30 -0400

    Changed freeipa config parameter name to avoid it getting overwritten by extra var

--- deploy/roles/treadmill-base-setup/templates/env_vars.sh
+++ deploy/roles/treadmill-base-setup/templates/env_vars.sh
@@ -2,6 +2,6 @@ export TREADMILL_ZOOKEEPER=zookeeper://foo@{% for instance in instance_facts_mas
 export TREADMILL_CELL={{vpc_id}}
 export TREADMILL_APPROOT={{app_root}}
 export TREADMILL_DNS_DOMAIN={{domain}}
-export TREADMILL_LDAP=ldap://{{ freeipa.name|lower }}1.{{ domain }}:1389
+export TREADMILL_LDAP=ldap://{{ freeipa_server.name|lower }}1.{{ domain }}:1389
 export TREADMILL_LDAP_SEARCH_BASE=ou=treadmill,{{ domain | regex_replace('(.*)\\.(.*)', 'dc=\\1,dc=\\2') }}
 alias z={{base_dir}}/zookeeper-3.4.9/bin/zkCli.sh
