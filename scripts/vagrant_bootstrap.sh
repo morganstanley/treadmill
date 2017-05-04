368c17feb6c9ae3e8b6f5403216c0950a8a6c891
Sny <sny.aggarwal@gmail.com>
2017-05-04 16:48:20 -0400

    vagrant provision now sources env vars

--- scripts/vagrant_bootstrap.sh
+++ scripts/vagrant_bootstrap.sh
@@ -6,3 +6,4 @@ echo 'password' > /home/treadmld/.treadmill_ldap
 service slapd start
 /home/centos/zookeeper-3.4.9/bin/zkServer.sh start
 export PYTHON_EGG_CACHE=/tmp/.python-eggs
+source /home/centos/treadmill/scripts/env_vars.sh
