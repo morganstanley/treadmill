ed98daffbd3cd7eb06e96eaea896bb147dcdc034
Sny <sny.aggarwal@gmail.com>
2017-05-04 15:03:59 -0400

    env_vars with ldap

--- scripts/env_vars.sh
+++ scripts/env_vars.sh
@@ -1,3 +1,5 @@
 export TREADMILL_ZOOKEEPER=zookeeper://foo@127.0.0.1:2181
 export TREADMILL_EXE_WHITELIST=/home/centos/treadmill/etc/linux.exe.config
-export TREADMILL_CELL=localhost.localdomain
+export TREADMILL_CELL=local
+export TREADMILL_LDAP=ldap://0.0.0.0:1389
+export TREADMILL_LDAP_SEARCH_BASE=ou=treadmill,dc=tw,dc=treadmill
