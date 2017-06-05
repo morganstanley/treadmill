400714dc1a619abda49b5893fa86b44f79664220
Ce Gao <ce.gao@outlook.com>
2017-06-05 11:16:20 +0800

    bench: Add two benches
    Signed-off-by: Ce Gao <ce.gao@outlook.com>

---
+++ research/500-app-from-500-to-1000-node-tree/500-app-from-500-to-1000-node-tree.r
@@ -0,0 +1,14 @@
+library(plotly)
+
+dt <- read.table("500-app-from-500-to-1000-node-tree.txt",
+                 col.names=c("number", "time"),
+                 fill=FALSE,
+                 strip.white=TRUE)
+
+l1 = lm(data = dt, time ~ number)
+l2 = lm(data = dt, time ~ I(number^2))
+
+p <- plot_ly(data = dt, x = ~number, y = ~time, name = 'raw data', type = 'scatter') %>% 
+  add_trace(y = predict(l1), mode = 'lines', name = 'linear')
+
+p
