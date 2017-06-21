41014851342480f83c4f46a51442fb0b0c55ca24
captiosus <captiosus@users.noreply.github.com>
2017-06-21 13:09:46 -0400

    bug_fixes & changes to psmem

--- vagrant/scripts/install-node-components.sh
+++ vagrant/scripts/install-node-components.sh
@@ -7,26 +7,12 @@ fi
 
 set -e
 
-function add_svc {
-    echo Adding service: $1
-    cp -v /home/vagrant/treadmill/vagrant/systemd/$1.service \
-        /etc/systemd/system/
-
-    /bin/systemctl daemon-reload
-    /bin/systemctl enable $1.service --now
-}
-
-function del_svc {
-    echo Deleting service: $1
-    /bin/systemctl disable $1.service --now || /bin/true
-    rm -vrf /etc/systemd/system/$1.service
-    /bin/systemctl daemon-reload
-}
-
 # Source environment variables.
 SCRIPTDIR=$(cd $(dirname $0) && pwd)
 
 source $SCRIPTDIR/env_vars.sh
+
+. $SCRIPTDIR/svc_utils.sh
 
 TM=/opt/treadmill/bin/treadmill
 
