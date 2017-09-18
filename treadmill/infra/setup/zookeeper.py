aa21fa8e2a246e154c5cfb0b50675e4154f999b5
Sny <sny.aggarwal@gmail.com>
2017-09-18 16:43:49 -0400

    Zk setup for IPA REST API
    * manages cluster setup with auto incremented IDX
    * manages hostname adds into ipa

--- treadmill/infra/setup/zookeeper.py
+++ treadmill/infra/setup/zookeeper.py
@@ -1,26 +1,51 @@
 from treadmill.infra.setup import base_provision
-from treadmill.infra import configuration, constants
+from treadmill.infra import configuration, constants, connection
 from treadmill.infra import instances
+from treadmill.api import ipa
 
 
 class Zookeeper(base_provision.BaseProvision):
-    def setup(self, image, key, cidr_block, instance_type, ldap_hostname,
+    def setup(self, image, key, cidr_block, instance_type,
               ipa_admin_password, subnet_id=None):
-        ipa_server_hostname = instances.Instances.get_ipa(
-            vpc_id=self.vpc.id
-        ).hostname
+        _hostnames = instances.Instances.get_hostnames_by_roles(
+            vpc_id=self.vpc.id,
+            roles=[
+                constants.ROLES['IPA'],
+                constants.ROLES['LDAP'],
+            ]
+        )
 
-        self.configuration = configuration.Zookeeper(
-            self.name,
-            ldap_hostname,
-            ipa_server_hostname
-        )
         self.subnet_name = constants.TREADMILL_CELL_SUBNET_NAME
-        super().setup(
-            image=image,
-            count=3,
-            cidr_block=cidr_block,
-            subnet_id=subnet_id,
-            key=key,
-            instance_type=instance_type
-        )
+        zk_hostnames = {
+            self.name + '1' + '.' + connection.Connection.context.domain: '1',
+            self.name + '2' + '.' + connection.Connection.context.domain: '2',
+            self.name + '3' + '.' + connection.Connection.context.domain: '3'
+        }
+        _ipa = ipa.API()
+
+        def _subnet_id(subnet_id):
+            if getattr(self, 'subnet', None) and self.subnet.id:
+                return self.subnet.id
+            else:
+                return subnet_id
+        _name = self.name
+        for _zk_h in zk_hostnames.keys():
+            _otp = _ipa.add_host({'hostname': _zk_h})
+            _idx = zk_hostnames[_zk_h]
+            self.configuration = configuration.Zookeeper(
+                ldap_hostname=_hostnames[constants.ROLES['LDAP']],
+                ipa_server_hostname=_hostnames[constants.ROLES['IPA']],
+                hostname=_zk_h,
+                otp=_otp,
+                idx=_idx
+            )
+
+            self.name = _name + _idx
+            super().setup(
+                image=image,
+                count=1,
+                cidr_block=cidr_block,
+                subnet_id=_subnet_id(subnet_id),
+                key=key,
+                instance_type=instance_type
+            )
