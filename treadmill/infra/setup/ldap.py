2e0b4fe12b4f6a5e1e1d0782a04d6869fc773b3d
Sny <sny.aggarwal@gmail.com>
2017-09-18 16:27:53 -0400

    LDAP setup updated
    * making it runnable via API from ipa server
    * adds hostname to ipa
    * takes care of unique hostname

--- treadmill/infra/setup/ldap.py
+++ treadmill/infra/setup/ldap.py
@@ -1,6 +1,7 @@
 from treadmill.infra.setup import base_provision
-from treadmill.infra import configuration
-from treadmill.infra import instances
+from treadmill.infra import configuration, connection, constants, instances
+from treadmill.api import ipa
+import time
 
 
 class LDAP(base_provision.BaseProvision):
@@ -13,23 +14,31 @@ class LDAP(base_provision.BaseProvision):
             tm_release,
             instance_type,
             app_root,
-            ldap_hostname,
             cell_subnet_id,
             ipa_admin_password,
             subnet_id=None
     ):
-        ipa_server_hostname = instances.Instances.get_ipa(
-            vpc_id=self.vpc.id
-        ).hostname
+        # TODO: remove count as parameter
+        count = 1
+        self.name = self.name + '-' + str(time.time())
+        hostname = self.name + '.' + connection.Connection.context.domain
+        otp = ipa.API().add_host({'hostname': hostname})
+
+        ipa_server_hostname = instances.Instances.get_hostnames_by_roles(
+            vpc_id=self.vpc.id,
+            roles=[
+                constants.ROLES['IPA']
+            ]
+        )[constants.ROLES['IPA']]
 
         self.configuration = configuration.LDAP(
             cell_subnet_id=cell_subnet_id,
-            ldap_hostname=ldap_hostname,
             tm_release=tm_release,
             app_root=app_root,
-            name=self.name,
+            hostname=hostname,
             ipa_admin_password=ipa_admin_password,
-            ipa_server_hostname=ipa_server_hostname
+            ipa_server_hostname=ipa_server_hostname,
+            otp=otp
         )
         super().setup(
             image=image,
