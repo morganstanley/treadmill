61f31e0acf5265ae93b08580b41955bdc5588312
Sny <sny.aggarwal@gmail.com>
2017-09-18 17:17:43 -0400

    fixing security_group port enable/disable tests | mocking request.urlopen

--- tests/infra/test_utils/test_security_group.py
+++ tests/infra/test_utils/test_security_group.py
@@ -4,30 +4,36 @@ from treadmill.infra.utils import security_group
 
 
 class SecuirtyGroupTest(unittest.TestCase):
+    @mock.patch('urllib.request.urlopen')
     @mock.patch('treadmill.infra.connection.Connection')
-    def test_enable(self, connectionMock):
+    def test_enable(self, connectionMock, requestMock):
+        http_response_mock = mock.Mock()
+        http_response_mock.read.side_effect = [b'so.me.i.p']
+        requestMock.return_value = http_response_mock
         conn = connectionMock()
-        security_group.my_ip = '127.0.0.1'
 
         security_group.enable(group_id='sg-123', port='11')
 
         conn.authorize_security_group_ingress.assert_called_once_with(
-            CidrIp='127.0.0.1',
+            CidrIp='so.me.i.p/32',
             FromPort=11,
             ToPort=11,
             GroupId='sg-123',
             IpProtocol='tcp'
         )
 
+    @mock.patch('urllib.request.urlopen')
     @mock.patch('treadmill.infra.connection.Connection')
-    def test_disable(self, connectionMock):
+    def test_disable(self, connectionMock, requestMock):
+        http_response_mock = mock.Mock()
+        http_response_mock.read.side_effect = [b'so.me.i.p']
+        requestMock.return_value = http_response_mock
         conn = connectionMock()
-        security_group.my_ip = '127.0.0.1'
 
         security_group.disable(group_id='sg-123', port='33')
 
         conn.revoke_security_group_ingress.assert_called_once_with(
-            CidrIp='127.0.0.1',
+            CidrIp='so.me.i.p/32',
             FromPort=33,
             ToPort=33,
             GroupId='sg-123',
