c1d1787d4d5bbc7d583f668df1b3f5d78528317d
Sny <sny.aggarwal@gmail.com>
2017-09-19 15:22:46 -0400

    ipa-server runs ipa cloud api modules on 5108

--- treadmill/infra/setup/scripts/install-ipa-server.sh
+++ treadmill/infra/setup/scripts/install-ipa-server.sh
@@ -56,8 +56,8 @@ chmod 755 /etc/cron.hourly/tmhostadm-kinit
 
 export TREADMILL_CELL="{{ CELL }}"
 
-nohup su -c "{{ TREADMILL }} sproc restapi -p 5108 --title 'Treadmill_IPA_API' \
-    -m ipa --cors-origin='.*'" tmhostadm > /var/log/ipa_api.out 2>&1 &
+nohup su -c "{{ TREADMILL }} sproc restapi -p 5108 --title 'Treadmill_API' \
+    -m ipa,cloud --cors-origin='.*'" tmhostadm > /var/log/ipa_api.out 2>&1 &
 
 TREADMLD_OUTPUT=$(ipa -n user-add --first=treadmld --last=proid --shell /bin/bash --class proid --random treadmld)
 TMP_TREADMLD_PASSWORD=$(echo "${TREADMLD_OUTPUT}" | grep 'Random password:' | awk '{print $3}')
